77e0b4763d31edc095e48948a80b38261179c65d
iamdeepz <deepakchandrasekaran14@gmail.com>
2021-12-03 23:56:09 +0530

    book slot ui completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardTimeSlotComponent } from './dashboard-time-slot/dashboard-time-slot.component';
+import { BookSlotComponent } from './book-slot/book-slot.component';
 
 const routes: Routes = [
   {path: 'home', component: DashboardTimeSlotComponent},
+  {path: 'book-slot', component: BookSlotComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DashboardTimeSlotComponent } from './dashboard-time-slot/dashboard-time-slot.component';
+import { BookSlotComponent } from './book-slot/book-slot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    DashboardTimeSlotComponent
+    DashboardTimeSlotComponent,
+    BookSlotComponent
   ],
   imports: [
     BrowserModule,
